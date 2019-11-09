752189fd46c2cd675f20895ee6fd83e60bc83299
oskari.kosonen <o.kosonen@gmail.com>
2019-11-09 16:43:53 +0200

    Initial implementation of model-view-update with Browser.sandbox

--- src/GameOfLife.elm
+++ src/GameOfLife.elm
@@ -1,29 +1,67 @@
 module GameOfLife exposing (main)
 
+import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, style)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 
 
-rows rowcount cellcount =
-    List.repeat rowcount (tr [] (cells cellcount))
+rows grid =
+    List.indexedMap (\index row -> tr [] (cells index row)) grid
 
 
-cells cellcount =
-    List.repeat cellcount (td [] [])
+cells rowIndex row =
+    List.indexedMap (\cellIndex val -> cell rowIndex cellIndex val) row
+
+
+cell rowIndex cellIndex val =
+    let
+        cls =
+            case val of
+                True ->
+                    "success"
+
+                False ->
+                    "danger"
+    in
+    td [ class cls ]
+        [ button [ class "btn", class ("btn-" ++ cls), onClick { description = "ClickedBox", row = rowIndex, col = cellIndex, val = not val } ] [ text (String.fromInt rowIndex ++ "-" ++ String.fromInt cellIndex) ]
+        ]
+
+
+type alias Model =
+    { grid : List (List Bool) }
+
+
+initialModel =
+    { grid = List.repeat 20 (List.repeat 20 False)
+    }
+
+
+view : Model -> Html Msg
+view model =
+    table [ class "table table-bordered table-condensed" ] [ tbody [] (rows model.grid) ]
+
+
+type alias Msg =
+    { description : String
+    , row : Int
+    , col : Int
+    , val : Bool
+    }
+
+
+update msg model =
+    if msg.description == "ClickedBox" then
+        { model | grid = List.repeat 20 (List.repeat 20 True) }
+
+    else
+        model
 
 
 main =
-    let
-        rowcount =
-            20
-
-        cellcount =
-            20
-
-        width =
-            String.fromInt (cellcount * 32) ++ "px"
-
-        height =
-            String.fromInt (rowcount * 32) ++ "px"
-    in
-    table [ class "table table-bordered table-condensed", style "width" width, style "height" height ] [ tbody [] (rows rowcount cellcount) ]
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
