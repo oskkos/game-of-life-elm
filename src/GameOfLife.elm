c4316e9292a0b57da1860d7fcbb057447e86f233
oskari.kosonen <o.kosonen@gmail.com>
2019-11-09 13:01:01 +0200

    Add some styling

--- src/GameOfLife.elm
+++ src/GameOfLife.elm
@@ -1,6 +1,7 @@
 module GameOfLife exposing (main)
 
 import Html exposing (..)
+import Html.Attributes exposing (class, style)
 
 
 rows rowcount cellcount =
@@ -8,8 +9,21 @@ rows rowcount cellcount =
 
 
 cells cellcount =
-    List.repeat cellcount (td [] [ text "O" ])
+    List.repeat cellcount (td [] [])
 
 
 main =
-    table [] (rows 20 20)
+    let
+        rowcount =
+            20
+
+        cellcount =
+            20
+
+        width =
+            String.fromInt (cellcount * 32) ++ "px"
+
+        height =
+            String.fromInt (rowcount * 32) ++ "px"
+    in
+    table [ class "table table-bordered table-condensed", style "width" width, style "height" height ] [ tbody [] (rows rowcount cellcount) ]
