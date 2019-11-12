b78fed3649410246741e9cedaee91d8f584b0831
oskari.kosonen <o.kosonen@gmail.com>
2019-11-12 19:11:10 +0200

    Implement automatically running mode with speed adjustment

--- src/GameOfLife.elm
+++ src/GameOfLife.elm
@@ -4,9 +4,10 @@ import Array exposing (Array)
 import Browser
 import CellToggle exposing (CellState(..), toggle)
 import Html exposing (..)
-import Html.Attributes exposing (class, style)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, style, type_, value)
+import Html.Events exposing (onClick, onInput)
 import NextTick exposing (tick)
+import Time
 
 
 rows grid =
@@ -32,20 +33,34 @@ cell rowIndex cellIndex val =
 
 
 type alias Model =
-    { grid : Array (Array CellState) }
+    { grid : Array (Array CellState), running : Bool, speed: Int }
 
 
 initialModel =
     { grid = Array.repeat 20 (Array.repeat 30 Dead)
+    , running = False
+    , speed = 100
     }
 
 
 view : Model -> Html Msg
 view model =
+    let
+        runningLabel =
+            case model.running of
+                True ->
+                    "Stop"
+
+                False ->
+                    "Start"
+    in
     div []
         [ table [ class "table table-bordered table-condensed", style "width" "initial" ] [ tbody [] (rows model.grid) ]
-        , button [ onClick Reset ] [ text "Reset" ]
-        , button [ onClick NextTick ] [ text "Next" ]
+        , button [ class "btn btn-primary", onClick Reset ] [ text "Reset" ]
+        , button [ class "btn btn-primary", onClick NextTick ] [ text "Next" ]
+        , button [ class "btn btn-primary", onClick ToggleRunning ] [ text runningLabel ]
+        , span [ style "padding" "5px"] [ text "Speed:"]
+        , input [ class "form-control-inline", style "width" "50px", type_ "number", onInput (ToggleSpeed << Maybe.withDefault 100 << String.toInt), value (String.fromInt model.speed)] []
         ]
 
 
@@ -53,23 +68,40 @@ type Msg
     = Toggle Int Int
     | Reset
     | NextTick
+    | ToggleRunning
+    | ToggleSpeed Int
 
 
 update msg model =
     case msg of
         Toggle x y ->
-            { model | grid = toggle model.grid x y }
+            ( { model | grid = toggle model.grid x y }, Cmd.none )
 
         Reset ->
-            initialModel
+            ( initialModel, Cmd.none )
 
         NextTick ->
-            { model | grid = tick model.grid }
+            ( { model | grid = tick model.grid }, Cmd.none )
+
+        ToggleRunning ->
+            ( { model | running = not model.running }, Cmd.none )
+        ToggleSpeed x ->
+            ( { model | speed = x }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    if model.running then
+        Time.every (toFloat model.speed) (always NextTick)
+
+    else
+        Sub.none
 
 
 main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
+    Browser.element
+        { init = \() -> ( initialModel, Cmd.none )
+        , subscriptions = subscriptions
         , update = update
+        , view = view
         }
